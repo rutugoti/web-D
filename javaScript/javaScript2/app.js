402b100c1aab1058dfe68cbd5717535fe4ad8d39
rutugoti <rutugotiwork24@gmail.com>
2026-02-17 14:01:00 +0530

    section2 done

---
+++ javaScript/javaScript2/app.js
@@ -0,0 +1,102 @@
+// console.log("hello world");
+// console.log("welcome to javaScript");
+
+// let a  = 10;
+// let b = 20;
+
+// console.log(a+b);
+
+// let pencil = 10;
+// let pen = 20;
+
+// console.log(`the total cost of pencil and pen is ${pencil + pen}`);
+
+
+//arethmetic operator
+// let a = 10;
+// let b = 20;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+//assignment operator
+
+// let a = 10;
+// a += 5; // a = a + 5
+// console.log(a);
+
+// let b = 20;
+// b -= 5; // b = b - 5
+// console.log(b);
+
+// let c = 30;
+// c *= 2; // c = c * 2
+// console.log(c);
+
+// let d = 40;
+// d /= 2; // d = d / 2
+// console.log(d);
+
+// let e = 50;
+// e %= 3; // e = e % 3
+// console.log(e);
+
+//comparison operator
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a == b); // false
+// console.log(a != b); // true
+// console.log(a > b); // false
+// console.log(a < b); // true
+// console.log(a >= b); // false
+// console.log(a <= b); // true
+
+//logical operator
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a > 5 && b > 15); // true
+// console.log(a > 15 || b > 15); // true
+// console.log(!(a > 5)); // false
+
+//conditional statement
+
+// let age = 18;
+
+// if(age >= 18){
+
+//     console.log("you are eligible to vote");
+// }else{
+
+//     console.log("you are not eligible to vote");
+// }
+
+// let marks = 85;
+
+// if(marks >= 90){
+
+//     console.log("grade A");
+// }else if(marks >= 80){  
+  //  console.log("grade B");
+// }else if(marks >= 70){
+
+//     console.log("grade C");
+// }else if(marks >= 60){
+
+//     console.log("grade D");
+// }else{
+
+//     console.log("grade F");
+// }
+
+// alert("something is wrong with you guys what happen???");
+
+let firstName =prompt("what is your name?");
+
+console.log(`hello ${firstName} welcome to javaScript`);
